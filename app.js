8e7de5ed8d162a1f568ab3e347e9c15991cceabc
mwlluis <mwlluis@hotmail.com>
2022-04-02 09:42:24 -0400

    app is ready

--- app.js
+++ app.js
@@ -21,13 +21,13 @@ form.addEventListener('submit', (e) => {
   const value  = parseInt(amount.value)
   const random = Math.floor(Math.random() * text.length)
   
-  if (isNaN(value) || value < 0 || value > 9) {
+  if (isNaN(value) || value <= 0 || value > 9) {
       result.innerHTML = `<p class="result">${text[random]}</p>`
   }
 
   else {
     
-    paragraphs = text.slice(0, value)
+    let paragraphs = text.slice(0, value)
     let outputArray = paragraphs.map( (paragraph) => {
       return `<p class="result">${paragraph}</p>`
     })
